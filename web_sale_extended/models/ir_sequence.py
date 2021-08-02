bea196bdcf34b1fbcfd6eb1dbd78933b22ad6fc9
FelipeNavasTodoo <ln@todoo.co>
2021-08-02 18:11:16 -0500

    [FIX] requiere beneficiaries number in sequence

--- web_sale_extended/models/ir_sequence.py
+++ web_sale_extended/models/ir_sequence.py
@@ -14,8 +14,6 @@ class IrSequence(models.Model):
     logo_website_pdf  = fields.Binary(string="Image del Plan para el documento PDF", readonly=False)
     logo_header_website_sponsor  = fields.Binary(string="Image del Plan del Patrocinador en el encabezado", readonly=False)
     logo_body_website_sponsor  = fields.Binary(string="Image del Patrocinador en el cuerpo del documento", readonly=False)
-    beneficiaries_number = fields.Integer(string="Número Máximo de Beneficiarios")
+    beneficiaries_number = fields.Integer(string="Número Máximo de Beneficiarios", required=True)
     sponsor_name = fields.Char('Nombre del Sponsor')
     sponsor_nit = fields.Char('Identificación del Sponsor')
-    sponsor_payment_url = fields.Char('URL de la Plataforma de Pagos')    
-    
